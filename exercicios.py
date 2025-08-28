7ae678c1e12fb0979269f99911e037ac873ce1b1
Drim <23.jsantos@gmail.com>
2025-08-27 22:57:56 -0300

    adicionado o exercício

---
+++ exercicios.py
@@ -0,0 +1,6 @@
+## imprimir a divisão inteira
+
+numero_1 = int(input('inserir um número inteiro: '))
+numero_2 = int(input('inserir o segundo número inteiro:  '))
+resultado = numero_1 // numero_2
+print(resultado)
